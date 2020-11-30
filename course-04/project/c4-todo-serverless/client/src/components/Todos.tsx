6cb486999cb6dca77e3bce6a83651abe477aa3aa
myturf <generalog@gmail.com>
2020-11-30 03:20:05 -0500

    Validation

--- course-04/project/c4-todo-serverless/client/src/components/Todos.tsx
+++ course-04/project/c4-todo-serverless/client/src/components/Todos.tsx
@@ -47,14 +47,17 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
   onTodoCreate = async (event: React.ChangeEvent<HTMLButtonElement>) => {
     try {
       const dueDate = this.calculateDueDate()
-      const newTodo = await createTodo(this.props.auth.getIdToken(), {
-        name: this.state.newTodoName,
-        dueDate
-      })
-      this.setState({
-        todos: [...this.state.todos, newTodo],
-        newTodoName: ''
-      })
+      if ( this.state.newTodoName ){
+        const newTodo = await createTodo(this.props.auth.getIdToken(), {
+          name: this.state.newTodoName,
+          dueDate
+        })
+        this.setState({
+          todos: [...this.state.todos, newTodo],
+          newTodoName: ''
+        })
+      }
+
     } catch {
       alert('Todo creation failed')
     }
